4eaadac1b868516d291f97403649624ed3af7bb0
dicey44 <226594125+dicey44@users.noreply.github.com>
2026-06-30 15:13:20 -0400

    chore: revise mangaApi

--- src/services/mangaApi.ts
+++ src/services/mangaApi.ts
@@ -1,11 +1,22 @@
+import type { Manga } from "../types/manga";
+
 const API_URL = "https://api.jikan.moe/v4";
 
-export async function searchManga(query: string) {
+export async function searchManga(query: string): Promise<Manga[]> {
+
     const response = await fetch(
         `${API_URL}/manga?q=${query}`
     );
 
     const data = await response.json();
 
-    return data;
+    return data.data.map((item: any ) => (
+        {
+            id: item.mal_id,
+            title: item.title,
+            imageUrl: item.images.jpg.image_url,
+            author: item.authors[0]?.name ?? "Unknown",
+            score: item.score,
+        }
+    ))
 }
