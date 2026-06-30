78a51c02586544727a30f8918e9259ef25401ff2
dicey44 <226594125+dicey44@users.noreply.github.com>
2026-06-30 15:42:46 -0400

    feat: add search function/input

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,11 @@ import type { Manga } from "./types/manga";
 function App() {
   const [mangaList, setMangaList] = useState<Manga[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     async function loadManga() {
-      const results = await searchManga("naruto");
+      const results = await searchManga("");
       setMangaList(results);
       setIsLoading(false);
     }
@@ -18,10 +19,24 @@ function App() {
     loadManga();
   }, []);
 
+  async function handleSearch() {
+    setIsLoading(true);
+    const results = await searchManga(query);
+
+    setMangaList(results);
+
+    setIsLoading(false)
+  }
+
 
   return (
     <>
       <h1>Manga Library</h1>
+      <input type="text" placeholder="Search manga..." value={query} onChange={(e) => setQuery(e.target.value)} />
+
+      <button onClick={handleSearch}>
+        Search
+      </button>
 
       {isLoading && <p>Loading manga...</p>}
 
